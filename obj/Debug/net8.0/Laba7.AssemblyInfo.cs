e0240bb391be904a61ace470099db583850cc488
Rubikon0703 <urasterxov0703@gmail.com>
2026-04-22 20:43:14 +0300

    6

--- obj/Debug/net8.0/Laba7.AssemblyInfo.cs
+++ obj/Debug/net8.0/Laba7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laba7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb767f8b6212094cf2f18dbbd909dda56d732e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9782c7eb0c404671277a1bdae0ecc1149b208f22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laba7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laba7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
